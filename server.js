83d226793af2a7a8ea36342e8a06b2f5b2297b59
Haroen Viaene <hello@haroen.me>
2016-09-15 18:37:44 +0200

    srs bsns

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
+const https = require('https');
+const util = require('./util.js');
 const constraints = require('./constraints.json');
+
+/**
+ * Assign a constraint to a member and a client
+ * @param  {Array} constraints All of the possible constraints
+ * @param  {Array} members     All of the joining members
+ * @param  {Object} client     The enquiring client
+ * @return {Object}            The chosen constraint, assignee and client
+ */
+function pick(constraints, members, client) {
+  return {
+    constraint: util.random(constraints),
+    assignee: util.random(members),
+    client: client
+  }
+}
+
+/**
+ * get the members who joined the bullgit pro™ program
+ * @param  {String} url the url where all of the members are defined (for example https://bullg.it/members.json)
+ * @return {Array}      All of the members with {pro: true}
+ */
+function getMembers(url) {
+  const availableMembers = [];
+  return availableMembers;
+}
 
 // answer to requests
 // fetch bullgit members (https://bullg.it/members.json)
---
+++ util.js
@@ -0,0 +1,14 @@
+module.exports = {
+  /**
+   * get a random value of an array
+   * @param  {Array} array the array to randomise
+   * @return {Object}      a random objec
+   */
+  random(array) {
+    if (!array instanceof Array) {
+      throw new Error('input was no array.', array);
+    } else {
+      return array[Math.floor(Math.random() * array.length)]
+    }
+  }
+};
