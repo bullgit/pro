a5bfa6d4bf53d41d766731718577ddb5517c9035
Haroen Viaene <hello@haroen.me>
2016-09-17 20:43:30 +0200

    use pretty bad way of posting

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const request = require('request');
+const dispatcher = require('httpdispatcher');
+const http = require('http');
 const util = require('./util.js');
 const constraints = require('./constraints.json');
-const dispatcher = require('httpdispatcher');
 
 /**
  * Assign a constraint to a member and a client
@@ -20,8 +21,8 @@ function pick(constraints, members, client) {
 
 /**
  * get the members who joined the bullgit pro™ program
- * @param  {String} url the url where all of the members are defined (for example https://bullg.it/members.json)
- * @return {Promise(Array)}     All of the members with {pro: true}
+ * @param  {String} url     The url where all of the members are defined (for example https://bullg.it/members.json)
+ * @return {Promise(Array)} All of the members with {pro: true}
  */
 function getMembers(url) {
   return new Promise(function(resolve, reject) {
@@ -45,9 +46,91 @@ function getMembers(url) {
 }
 
 getMembers('https://bullg.it/members.json').then(members => {
-  console.log(pick(constraints, members, 'client'));
+  // console.log(pick(constraints, members, 'client'));
 }).catch(err => {
   console.error(err);
+});
+
+
+function serve(port) {
+  console.log('serving');
+  return new Promise((resolve, reject) => {
+    port = port || 8000;
+
+    // handle a request
+    function handleRequest(request, response) {
+      try {
+        // log the request on console
+        // TODO: delete this in prod
+        console.log(request.url);
+        // Dispatch
+        dispatcher.dispatch(request, response);
+      } catch (err) {
+        // TODO: use sentry
+        console.error(err);
+      }
+    }
+
+    dispatcher.onPost('/request', function(req, res) {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(`Got Post Data: ${JSON.stringify(req.params)}`);
+    });
+
+    // serve the index
+    // dispatcher.onGet('/', (req, res) => {
+    //   const options = {};
+    //   const data = {
+    //     title,
+    //     source
+    //   };
+    //   res.end(pug.renderFile(path.join(__dirname, '/../lib/index.pug'), {
+    //     options,
+    //     data
+    //   }));
+    // });
+
+    dispatcher.onGet('/', (req, res) => {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(`
+<form action="/request" method="post">
+  <input type="text" id="title" name="title" placeholder="title">
+  <textarea name="message" id="message" placeholder="message"></textarea>
+  <input type="submit">
+</form>`);
+    });
+
+    // create a server
+    const server = http.createServer(handleRequest);
+
+    // start the server
+    server.listen(port, () => {
+      resolve(`Server listening on: ${port}`);
+    });
+
+    server.on('error', err => {
+      switch (err.code) {
+        case 'EACCES':
+          reject('You don\'t have enough permissions to run on this port. Try running this with sudo.');
+          break;
+        case 'EADDRINUSE':
+          reject('This address (or port) is already in use. Try changing the port.');
+          break;
+        default:
+          reject('There was an error with code', err.code, '\nand message', err.message);
+          break;
+      }
+    });
+  });
+}
+
+serve(8008).then(res => {
+  console.log(res);
+}).catch(err => {
+  console.error(`There was an error serving the provided datafeed:\n${err}`);
 });
 
 // answer to requests
